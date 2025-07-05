469849a86db300f742435b7758bb1afcaf51032f
vibhorverma24 <vibhorverma8790@gmail.com>
2025-07-05 23:18:39 +0530

    Added comment in filename.js

--- server/routes/leave.js
+++ server/routes/leave.js
@@ -10,4 +10,6 @@ router.get('/:id/:role', authMiddleware, getLeave)
 router.get('/',authMiddleware,getLeaves)
 router.put('/:id', authMiddleware, updateLeave)
 
+//an jknsjns
+
 export default router
